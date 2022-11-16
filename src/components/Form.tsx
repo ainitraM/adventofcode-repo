9139760df30a64eabe10946d41821d1021833087
Martyna Zagrabska <martyna.zagrabska2@klarna.com>
2022-11-16 23:18:15 +0100

    Correct form UI

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,35 +1,51 @@
 import React from 'react';
 import axios from 'axios';
+import banner from "../assets/klarna-banner-3-3.png";
 
 interface ModalType {
   isOpen: boolean;
   toggle: () => void;
 }
 
-export default function Form(props: ModalType) {
+export default function Form (props: ModalType) {
   const [formStatus, setFormStatus] = React.useState('Send');
   const [form, setForm] = React.useState({});
+
+  const escFunction = React.useCallback((event: { key: string; }) => {
+    if (event.key === "Escape") {
+      if (props.isOpen) props.toggle();
+    }
+  }, [props.isOpen]);
 
   const handleForm = (name: string, value: string) => {
     setForm({...form, [name]: value});
   }
 
+  React.useEffect(() => {
+    document.addEventListener("keydown", escFunction, false);
+
+    return () => {
+      document.removeEventListener("keydown", escFunction, false);
+    };
+  }, []);
+
   const onSubmit = async () => {
     console.log(form);
     try {
-      setFormStatus('Submitting...')
-      const response = await axios.post('https://sheet.best/api/sheets/7b203d8b-41bf-4e81-a469-fb714033d1f9',
+      setFormStatus('Submitting...');
+      await axios.post('https://sheet.best/api/sheets/7b203d8b-41bf-4e81-a469-fb714033d1f9',
         form);
     } catch (err) {
-      setFormStatus('Something went wrong. Retry.')
+      setFormStatus('Something went wrong. Retry.');
     } finally {
       setFormStatus('Done')
-      setTimeout(() => setFormStatus('Send'), 10000)
+      setTimeout(() => setFormStatus('Send'), 10000);
     }
   }
   return (
     <div className="form-modal">
       <span className="close" onClick={props.toggle}>&times;</span>
+      <legend className="form-header">Register</legend>
       <div className="modal-content">
         <form id="userForm" onSubmit={onSubmit}>
           <label className="input-label">Full Name</label>
