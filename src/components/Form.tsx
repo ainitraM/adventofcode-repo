85e971c8045d1887b70768d44320aa2fe6b6b9a0
Martyna Zagrabska <martyna.zagrabska2@klarna.com>
2023-11-27 18:15:41 +0100

    Form fix

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import axios from 'axios';
 import { spreadsheetAPI, ModalType } from "../consts";
 
 export default function Form (props: ModalType) {
   const [formStatus, setFormStatus] = React.useState('Send');
   const [form, setForm] = React.useState({});
+  const [register, setRegister] = React.useState('Register');
 
   const escFunction = React.useCallback((event: { key: string; }) => {
     if (event.key === "Escape") {
@@ -24,43 +24,45 @@ export default function Form (props: ModalType) {
     };
   }, []);
 
-  const onSubmit = async () => {
-    console.log(form);
+  async function appendSpreadsheet() {
     setFormStatus('Submitting...');
     fetch(spreadsheetAPI, {
-      method: "POST",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(form),
-    })
-    .then((r) => r.json())
-    .then((form) => {
-      console.log(form);
-    })
-    .catch((error) => {
-      setFormStatus('Something went wrong. Retry.');
-   })
-   .finally(() => {
-    setFormStatus('Done')
-      setTimeout(() => setFormStatus('Send'), 10000);
-   });
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(Object.values(form))
+      }).then(() => {
+      setFormStatus('Done')
+      setRegister('Thank you for signing up!');
+      setTimeout(() => {
+        if (props.isOpen) props.toggle();
+      }, 2000);
+    }).catch(() => {
+      setFormStatus('Something went wrong. Retry and contact admins.');
+    });
   }
+
+  function onSubmit (e: { preventDefault: () => void; }) {
+    e.preventDefault();
+    appendSpreadsheet();
+  }
+
   return (
     <div className="form-modal">
       <span className="close" onClick={props.toggle}>&times;</span>
-      <legend className="form-header">Register</legend>
+      <legend className="form-header">{register}</legend>
       <div className="modal-content">
         <form id="userForm" onSubmit={onSubmit}>
           <label className="input-label">Full Name</label>
-          <input className="input" name="Full-Name" onChange={(e)=>handleForm('Full-Name', e.target.value)}/>
+          <input required className="input" name="Full-Name" onChange={(e)=>handleForm('Full-Name', e.target.value)}/>
           <label className="input-label">AoC Nickname</label>
-          <input className="input" name="Nickname" onChange={(e)=>handleForm('Nickname', e.target.value)}/>
+          <input required className="input" name="Nickname" onChange={(e)=>handleForm('Nickname', e.target.value)}/>
           <label className="input-label">Email</label>
-          <input className="input" name="Email" onChange={(e)=>handleForm('Email', e.target.value)}/>
+          <input required className="input" name="Email" onChange={(e)=>handleForm('Email', e.target.value)}/>
           <label className="input-label">Coding language</label>
-          <input className="input" name="Language" onChange={(e)=>handleForm('Language', e.target.value)}/>
+          <input required className="input" name="Language" onChange={(e)=>handleForm('Language', e.target.value)}/>
           <button className="submit-button" type="submit" >
             { formStatus }
           </button>
